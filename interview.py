b6ca1a6a9c05c155e766d3f70766738d04a842d5
Parth Shah <parth.shah@berkeley.edu>
2018-08-25 17:03:17 -0700

    adding files

---
+++ interview.py
@@ -0,0 +1,44 @@
+left = []
+middle = []
+right = []
+
+def create_towers(num_disks):
+	pass
+	#Replace and Fill In Code Here
+
+def get_num_disks():
+	return int(input("How many disks do you want to play with?\n"))
+
+def get_user_input():
+	pass
+	#Replace and Fill In Code Here
+
+	
+
+def print_towers():
+	print("\nCurrent Towers....\n")
+	print("Left: {0}".format(left))
+	print("Middle: {0}".format(middle))
+	print("Right: {0}".format(right))
+
+def is_valid_move(from_tower, to_tower):
+	pass
+	#Replace and Fill In Code Here
+
+def is_solved(num_disks):
+	pass
+	#Replace and Fill In Code Here
+
+def play_game():
+	num_disks = get_num_disks()
+	create_towers(num_disks)
+	#Fill In Code Here
+
+if __name__ == "__main__": play_game()
+
+
+
+
+
+	
+
